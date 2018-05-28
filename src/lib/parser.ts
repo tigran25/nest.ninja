a8550aa04d6689571b4b3ffa6c4faa9217dd5938
tigran25 <tigranvetisyan@gmail.com>
2018-05-28 16:43:22 +0100

    comments

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,5 +1,16 @@
 import * as cheerio from "cheerio"
 
+// interface Point {
+//   x: number
+//   y: number
+// }
+// type Polygon = [Point]
+
+// export function polygonify(el:SVGElement):Polygon {
+//   SVGtoWKT.convert(el)
+//   return [{ x: 0, y: 0 }]
+// }
+
 class Parser {
   private $: CheerioStatic
 
